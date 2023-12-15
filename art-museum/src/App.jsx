a9c4d5189664868230baf1e60a6f7af5c54218e1
Carmen <carmenshiu07@gmail.com>
2023-12-14 16:20:01 -0800

    phase 3 complete

--- art-museum/src/App.jsx
+++ art-museum/src/App.jsx
@@ -1,24 +1,37 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import harvardArt from './data/harvardArt';
 import GalleryNavigation from './components/GalleryNavigation/GalleryNavigation';
 
+function Layout() {
+  return (
+    <div>
+      <GalleryNavigation galleries={harvardArt.records} />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
+
 const router = createBrowserRouter([
   {
-    path: '*',
-    element: <h2>Page Not Found</h2>
-  },
-  {
-    path: '/',
-    element: <>
-      <h2>Harvard Art Museum</h2>
-      <p>Look, but Don&apos;t Touch. Please select a Gallery in the navigation bar.</p>
-    </>
-  },
-  {
-    path: '/galleries/',
-    element: <GalleryNavigation galleries={harvardArt.records} />
+    element: <Layout />,
+    children: [
+      {
+        path: '*',
+        element: <h2>Page Not Found</h2>
+      },
+      {
+        path: '/',
+        element: <>
+          <h2>Harvard Art Museum</h2>
+          <p>Look, but Don&apos;t Touch. Please select a Gallery in the navigation bar.</p>
+        </>
+      }
+    ]
   }
 ])
+
 function App() {
   return <RouterProvider router={router} />;
 }
--- art-museum/src/components/GalleryNavigation/GalleryNavigation.jsx
+++ art-museum/src/components/GalleryNavigation/GalleryNavigation.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import './GalleryNavigation.css'
 
 export default function GalleryNavigation({ galleries }) {
     return (
